120a3409830f0e541c5b91b5f8861273fdce8767
Jeremy Pepper <jpepper1@gmail.com>
2016-04-29 03:21:06 +0000

    mapping::truncate(): update size

--- mapping.cc
+++ mapping.cc
@@ -44,6 +44,8 @@ void mapping::truncate(size_t size) {
 	void *drop = reinterpret_cast<char *>(_addr) + size;
 	if(munmap(drop, _length - size))
 		throw std::runtime_error(strerror(errno));
+	_length = size;
+	_valid_length = size;
 }
 
 long mapping::page_size() {
